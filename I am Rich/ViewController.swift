e2d9c96c5bf02d59f5460c2245868abbc87b1b88
Syed Shakeeb <49868935+firas98@users.noreply.github.com>
2019-05-04 15:10:19 +0530

    Add files via upload

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Syed Shakeeb on 04/05/19.
+//  Copyright © 2019 Syed Shakeeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
